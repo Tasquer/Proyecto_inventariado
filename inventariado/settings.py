ca89fd3902faabddc2719793f9b8d0d6d321557d
1zcan <orlandoaravenav568@gmail.com>
2025-11-09 13:45:35 -0300

    cambios en version de django por compatibilidad con xampp

--- inventariado/settings.py
+++ inventariado/settings.py
@@ -93,8 +93,8 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASS'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT', default=3306, cast=int),
+        'HOST': 'localhost',
+        'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
