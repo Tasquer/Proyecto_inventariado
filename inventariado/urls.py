47b6fe171aeb33e882f076cb36be978aa6d28934
tasquer <tasquerfdw@gmail.com>
2025-11-10 16:43:43 -0300

    version django modificada, path de login usuario listo

--- inventariado/urls.py
+++ inventariado/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('usuarios/', include('usuarios.urls')),  # Incluimos las URLs de la app 'usuarios'
+    path('usuarios/', include('usuarios.urls')), # Incluimos las URLs de la app 'usuarios'
+    path('', RedirectView.as_view(pattern_name='usuarios:login', permanent=True)),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
